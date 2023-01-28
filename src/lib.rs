e79059c5c9cd829c6f6fa4c8a0c907d14ae52bc9
Dominik Fluch <dominik@diefluchs.at>
2023-01-28 20:36:11 +0100

    Add basic native-bind structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! This is the coda programming language native crate. Coda is a modern, general purpose programming language.
+
+/// Enum holding all native coda values.
+#[derive(Debug, Clone)]
+pub enum CodaValue {
+    Character(char),
+    Long(i128),
+    Integer(i32),
+    Double(f64),
+    Byte(u8),
+    Boolean(bool),
+    String(String)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Enum containing all possible control-flow impacts a [NativeBind] could have.
+#[derive(Debug, Clone)]
+pub enum ControlFlowImpact {
+    Return(CodaValue),
+    Break,
+    Continue
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Struct representing a coda native-bind, offering an interface between
+/// coda code and rust-compiled native code.
+#[derive(Debug, Clone, Hash)]
+pub struct NativeBind {
+    /// The name/identifier of the [NativeBind].
+    pub name: String,
+    /// The function handling the interfacing.
+    pub handler: fn(Vec<CodaValue>) -> ControlFlowImpact
 }
